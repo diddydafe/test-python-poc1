02358de14fd8d2c6bd6525706948e22c1b86e035
SupremeOverlord <fluid0028@gmail.com>
2025-05-19 14:23:28 -0500

    Update bad password

---
+++ bad_password.py
@@ -0,0 +1,9 @@
+# bad_password.py
+
+# This is an example of a hardcoded password (security issue)
+username = "admin"
+password = "password"  # This should be flagged by Semgrep
+
+# This is a safer way and should NOT be flagged
+import getpass
+secure_password = getpass.getpass("Enter your password: ")
